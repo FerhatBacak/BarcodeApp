864f81f1d26d1f1c2a1205b605f89e4c7ac3ecf3
FerhatBacak <136471177+FerhatBacak@users.noreply.github.com>
2023-07-02 20:52:29 +0300

    Add files via upload
    BarcodeApp C# WindowsForm code

---
+++ BarcodeApp/BarcodeApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace BarcodeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox1.Image = global::BarcodeApp.Properties.Resources.elTerminali2;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(386, 513);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.lblMsg);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(76, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 307);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 66);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(28, 216);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(16, 13);
+            this.lblMsg.TabIndex = 6;
+            this.lblMsg.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Barkod Okutunuz";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Gönder(Sp)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 537);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblMsg;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BarcodeApp/BarcodeApp/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BarcodeApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string cs = "DATA SOURCE=.;INITIAL CATALOG=PRODUCTION;INTEGRATED SECURITY=TRUE";
+        SqlConnection conn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+  
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.ConnectionString = cs;
+           string sql = "SELECT * FROM PRODUCTION.DBO.BARCODES WHERE BARCODE='" + textBox1.Text + "'";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            da.SelectCommand = cmd;
+            ds.Clear();
+            ds.Reset();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count==0) 
+            {
+                lblMsg.Text="Böyle bir barkod bulunamadı!";
+                return;
+            }
+
+            sql = "SELECT * FROM PRODUCTION.DBO.PRODPLAN WHERE ITEMID IN(SELECT ITEMID FROM BARCODES WHERE BARCODE='" + textBox1.Text + "')"; 
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            da.SelectCommand = cmd;
+            ds.Clear();
+            ds.Reset();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count==0)
+            {
+                lblMsg.Text = "Bu ürüne ait üretim planı bulunamadı!";
+                return;
+            }
+
+            sql = "SELECT * FROM PRODUCTION.DBO.BARCODE_PRODUCTION WHERE BARCODE='" + textBox1.Text + "'";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            da.SelectCommand = cmd;
+            ds.Clear();
+            ds.Reset();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                lblMsg.Text = "Bu barkod daha önce okutulmuş!";
+                return;
+            }
+
+            sql = "INSERT INTO PRODUCTION.DBO.BARCODE_PRODUCTION (BARCODE,DATE_,STATION) VALUES ('" + textBox1.Text + "',GETDATE(),1)";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            da.SelectCommand = cmd;
+            ds.Clear();
+            ds.Reset();
+            da.Fill(ds);
+            lblMsg.Text = "Barkod başarı ile eklendi";
+            textBox1.Text = "";
+            textBox1.Focus();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.ConnectionString = cs;
+                string sql = "EXEC BARCODE_INSERT @BARCODE='" + textBox1.Text + "',@STATIONID=1";
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sql;
+                da.SelectCommand = cmd;
+                ds.Clear();
+                ds.Reset();
+                da.Fill(ds);
+                lblMsg.Text = "Barkod başarı ile eklenmiştir.";
+                lblMsg.Text = "";
+                lblMsg.Focus();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+}
